343c794556926bd11084efa8d1354329942ca99f
mamagalh <matheusbaeck@gmail.com>
2023-03-06 02:16:05 +0100

    gnl done 42

--- get_next_line/extra/main.c
+++ get_next_line/extra/main.c
@@ -6,7 +6,7 @@
 /*   By: mamagalh@student.42madrid.com <mamagalh    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 22:23:32 by mamagalh@st       #+#    #+#             */
-/*   Updated: 2023/03/05 18:58:37 by mamagalh@st      ###   ########.fr       */
+/*   Updated: 2023/03/06 01:55:31 by mamagalh@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ void	find_leaks(void)
 	system("leaks -q main.out");
 }
 
-
-int run_test(char *cfd, char *cfd_exit)
+int	run_test(char *cfd, char *cfd_exit)
 {
 	char	*str;
 	int		count;
@@ -46,12 +45,183 @@ int run_test(char *cfd, char *cfd_exit)
 			else
 			{
 				write(1, cfd, 11);
-				write(1, "    exit 2 NULL\n", 16);
+				write(1, "    exit 1 NULL\n", 16);
 				free(str);
 				return (1);
 			}
 		}
+		else if ((count % 2) == 0)
+		{
+			write(1, cfd, 11);
+			write(1, "    exit 2 NULL\n", 16);
+			system("leaks -q main.out");
+			return (2);
+		}
+		count++;
+	}
+	free(str);
+	return (0);
+}
+
+int	run_test_bad(char *cfd, char *cfd_exit)
+{
+	char	*str;
+	int		count;
+	int		fd;
+	int		fd_exit;
+	int		bad_fd;
+
+	//atexit(find_leaks);
+	fd = open(cfd, 2);
+	fd_exit = open(cfd_exit, 2);
+	bad_fd = 5;
+	count = 0;
+	while (count <= 2147483646)
+	{
+		if ((count % 2) == 0)
+		{
+			str = get_next_line(fd);
+		}
 		else
+		{
+			str = get_next_line(bad_fd);
+		}
+		if (str)
+		{
+			if (*str)
+			{
+				write(fd_exit, str, strlen(str));
+				free(str);
+			}
+			else
+			{
+				write(1, cfd, 11);
+				write(1, "    exit 1 NULL\n", 16);
+				free(str);
+				return (1);
+			}
+		}
+		else if ((count % 2) == 0)
+		{
+			write(1, cfd, 11);
+			write(1, "    exit 2 NULL\n", 16);
+			system("leaks -q main.out");
+			return (2);
+		}
+		count++;
+	}
+	free(str);
+	return (0);
+}
+
+// int	run_test_bonus(char *cfd, char *cfd_exit, char *cfd2, char *cfd_exit2)
+// {
+// 	char	*str;
+// 	char	*str2;
+// 	int		count;
+// 	int		end;
+// 	int		fd;
+// 	int		fd_exit;
+// 	int		fd2;
+// 	int		fd_exit2;
+
+// 	fd = open(cfd, 2);
+// 	fd_exit = open(cfd_exit, 2);
+// 	fd2 = open(cfd2, 2);
+// 	fd_exit2 = open(cfd_exit2, 2);
+// 	count = 0;
+// 	end = 0;
+// 	while ((end / 5) == 1)
+// 	{
+// 		if ((count % 2) == 0 && (end / 2) == 0)
+// 		{
+// 			str = get_next_line(fd);
+// 			if (str)
+// 			{
+// 				if (*str)
+// 				{
+// 					write(fd_exit, str, strlen(str));
+// 					free(str);
+// 				}
+// 			}
+// 			else if ((count % 2) == 0)
+// 			{
+// 				write(1, cfd, 11);
+// 				write(1, "    exit 2 NULL\n", 16);
+// 				end += 2;
+// 			}
+// 		}
+// 		else if ((end / 3) == 0)
+// 		{
+// 			str2 = get_next_line(fd2);
+// 			if (str2)
+// 			{
+// 				if (*str2)
+// 				{
+// 					write(fd_exit2, str2, strlen(str2));
+// 					free(str2);
+// 				}
+// 				else
+// 				{
+// 					write(1, cfd, 11);
+// 					write(1, "    exit 2 NULL\n", 16);
+// 					free(str2);
+// 					return (1);
+// 				}
+// 			}
+// 			else if ((count % 2) == 0)
+// 			{
+// 				write(1, cfd, 11);
+// 				write(1, "    exit 2 NULL\n", 16);
+// 				end += 3;
+// 			}
+// 		}
+// 		count++;
+// 	}
+// 	system("leaks -q main.out");
+// 	return (0);
+// }
+
+int	run_test_bonus(char *cfd, char *cfd_exit, char *cfd2, char *cfd_exit2)
+{
+	char	*str;
+	char	*str2;
+	int		count;
+	int		end;
+	int		fd;
+	int		fd_exit;
+	int		fd2;
+	int		fd_exit2;
+
+	fd = open(cfd, 2);
+	fd_exit = open(cfd_exit, 2);
+	fd2 = open(cfd2, 2);
+	fd_exit2 = open(cfd_exit2, 2);
+	count = 0;
+	end = 0;
+	count = 0;
+	while (count <= 2147483646)
+	{
+		str = get_next_line(fd);
+		str2 = get_next_line(fd2);
+		if (str)
+		{
+			if (*str)
+			{
+				write(fd_exit, str, strlen(str));
+				write(fd_exit2, str2, strlen(str2));
+				free(str);
+				free(str2);
+			}
+			else
+			{
+				write(1, cfd, 11);
+				write(1, "    exit 1 NULL\n", 16);
+				free(str);
+				return (1);
+			}
+		}
+		else if ((count % 2) == 0)
 		{
 			write(1, cfd, 11);
 			write(1, "    exit 2 NULL\n", 16);
@@ -76,5 +246,8 @@ int	main(void)
 	run_test("files/file6.txt" , "files/file6_exit.txt");
 	run_test("files/file7.txt" , "files/file7_exit.txt");
 	run_test("files/file8.txt" , "files/file8_exit.txt");
+	run_test_bad("files/file9.txt" , "files/file9_exit.txt");
+	run_test_bonus("files/file11.txt" , "files/file11_exit.txt"
+		, "files/file12.txt" , "files/file12_exit.txt");
 	return (0);
 }
